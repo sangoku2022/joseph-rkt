3261ec6a211e7ebecb2b224bb6399dee82b875eb
sangoku2022 <josephleec@gmail.com>
2022-02-26 10:25:56 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+         
+    
+        <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+        Welcome To Rabbit King Token Project  <br />  </h1> <h2 classNAme= "text-3xl sm:text-5xl text-white text-gradient py-1">A decentralized utility token for rabbit industry
+          </h2> <p>  </p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
